cf08537d61ce1819d98886e01f00aa4f7ade8bc4
Alex Henry <tukkek@gmail.com>
2024-03-09 00:00:00 +0000

    Python 3 support for pymusanim.sh

--- pymusanim.sh
+++ pymusanim.sh
@@ -1,14 +1,16 @@
 #!/bin/bash
 # given the same parameters as PyMusAnimLauncher, runs the whole process of creating a video
 function checktime() { now=`date +%s`;}
+
 function printtime(){
     checktime
     timeelapsed=$(($now-$allStart))
     echo "Total time: $(($timeelapsed/60)) minutes ($timeelapsed seconds)"
 }
+
 function animate(){
-    echo python MusAnimLauncher.py $* #TODO
-    python MusAnimLauncher.py $* #first try
+    echo python3 MusAnimLauncher.py $* #TODO
+    python3 MusAnimLauncher.py $* #first try
     mkdir $2
     cp $1 $2
     if ! [ -e "$2/frame00000.png" ]; then
@@ -16,8 +18,8 @@ function animate(){
         echo "Converting MIDI..."
         converted="$2/musescore.mid"
         mscore "$1" -o $converted #convert mid->mid 2> /dev/null
-        echo python MusAnimLauncher.py $converted $2 $3 #TODO
-        python MusAnimLauncher.py $converted $2 $3 #second try
+        echo python3 MusAnimLauncher.py $converted $2 $3 #TODO
+        python3 MusAnimLauncher.py $converted $2 $3 #second try
         if ! [ -e "$2/frame00000.png" ]; then exit 1; fi #second try failed
     fi
 }
